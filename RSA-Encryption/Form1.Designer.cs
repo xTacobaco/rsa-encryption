2a0552399cdb78dffb556755be18edee4bfcd2fa
16peak <16peak@stockholmscience.se>
2019-03-27 13:21:13 +0100

    Test1

--- RSA-Encryption/Form1.Designer.cs
+++ RSA-Encryption/Form1.Designer.cs
@@ -23,16 +23,16 @@
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
-      this.SuspendLayout();
-      // 
-      // Form1
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(566, 316);
-      this.Name = "Form1";
-      this.Text = "Form1";
-      this.ResumeLayout(false);
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 424);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
 
     }
 
