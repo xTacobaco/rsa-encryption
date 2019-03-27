d9336727264cde4ccb02991111ac582accc7346e
16peak <16peak@stockholmscience.se>
2019-03-27 13:38:58 +0100

    Screen change test

--- RSA-Encryption/Form1.Designer.cs
+++ RSA-Encryption/Form1.Designer.cs
@@ -29,7 +29,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(473, 424);
+            this.ClientSize = new System.Drawing.Size(594, 470);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
