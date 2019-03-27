8323147e556cf30bb638a7d8622f6c8b96dce99c
William Beinö <16wibe@stockholmscience.se>
2019-03-27 13:54:54 +0100

    lade till kommentar i GreatestCommonDivider

--- RSA-Encryption/Algorithms/EuclideanAlgorithm.cs
+++ RSA-Encryption/Algorithms/EuclideanAlgorithm.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 namespace RSA_Encryption {
   class EuclideanAlgorithm {
     public int GreatestCommonDivider(int a, int b) {
-      int k = a / b;
+      int k = a / b; //k = a / b
       int r = a - k * b;
-
+      
       while (r > 0) {
           a = b;
           b = r;
