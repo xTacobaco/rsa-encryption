c9ad634f7b0414a6eb88d44d27b968ab4b91159c
16peak <16peak@stockholmscience.se>
2019-03-29 10:11:42 +0100

    start med encrytpion,lite dio

--- RSA-Encryption/Algorithms/EuclideanAlgorithm.cs
+++ RSA-Encryption/Algorithms/EuclideanAlgorithm.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace RSA_Encryption {
   class EuclideanAlgorithm {
+        //Värdena som kommer in är: Första gången är m och e
     public int GreatestCommonDivider(int a, int b) {
-      int k = a / b; //k = a / b
+      int k = a / b; //imed k hugger bort allt efter decimaltecknet
       int r = a - k * b;
       
       while (r > 0) {
