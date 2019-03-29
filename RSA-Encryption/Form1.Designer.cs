a9baabe8f45e2bfdc7cafc532452834524c8fd48
William Beinö <16wibe@stockholmscience.se>
2019-03-29 10:12:37 +0100

    form layout

--- RSA-Encryption/Form1.Designer.cs
+++ RSA-Encryption/Form1.Designer.cs
@@ -23,20 +23,80 @@
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(594, 470);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+      this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+      this.lbl = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(155, 155);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(232, 20);
+      this.textBox1.TabIndex = 0;
+      // 
+      // numericUpDown1
+      // 
+      this.numericUpDown1.Location = new System.Drawing.Point(155, 45);
+      this.numericUpDown1.Name = "numericUpDown1";
+      this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+      this.numericUpDown1.TabIndex = 1;
+      // 
+      // numericUpDown2
+      // 
+      this.numericUpDown2.Location = new System.Drawing.Point(155, 71);
+      this.numericUpDown2.Name = "numericUpDown2";
+      this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+      this.numericUpDown2.TabIndex = 2;
+      // 
+      // lbl
+      // 
+      this.lbl.AutoSize = true;
+      this.lbl.Location = new System.Drawing.Point(62, 47);
+      this.lbl.Name = "lbl";
+      this.lbl.Size = new System.Drawing.Size(87, 13);
+      this.lbl.TabIndex = 3;
+      this.lbl.Text = "Prime number #1";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(62, 73);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(87, 13);
+      this.label2.TabIndex = 4;
+      this.label2.Text = "Prime number #2";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(594, 470);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.lbl);
+      this.Controls.Add(this.numericUpDown2);
+      this.Controls.Add(this.numericUpDown1);
+      this.Controls.Add(this.textBox1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
+
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.NumericUpDown numericUpDown1;
+    private System.Windows.Forms.NumericUpDown numericUpDown2;
+    private System.Windows.Forms.Label lbl;
+    private System.Windows.Forms.Label label2;
   }
 }
 
